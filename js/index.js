b320c2124d32f86bf692a2167d618303e23f1d7c
nbressn1 <nbressn1@binghamton.edu>
2017-10-18 09:09:57 -0400

    Revert "remove js"
    This reverts commit d3648f01699b6b79e814bb6e3fe7ce84f9ef0526.

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+$(function() {
+    waypoints.init();
+})
+
+var waypoints = {
+    init: function() {
+        // container - used for toggling classes to animate content
+        this.container = $('html');
+
+        // header - main header container: an inner wrapper element should get fixed (.header-bar)
+        this.header = $('[data-animate-header-container]');
+
+        // headerHeight - the inner header element that gets fixed
+        this.headerHeight = $('[data-animate-header]').outerHeight();
+
+        // targetElement - when this element hits top of screen the header  animates
+        this.targetElement = $('[data-animate-header-target]');
+
+        // bindControls
+        this.bindControls();
+    },
+    animateHeader: function() {
+        var self = this;
+
+        // Add class when header is in/out of view
+        self.header.waypoint(function(direction) {
+            if (direction === "up") {
+                self.container.removeClass('header-past header-hide');
+            } else {
+                self.container.addClass('header-past');
+            }
+        }, { offset: -self.headerHeight });
+
+        // Animate header in when this element hits top of screen
+        self.targetElement.waypoint(function(direction) {
+            if (direction === "up") {
+                self.container.addClass('header-hide').removeClass('header-show');
+            } else {
+                self.container.addClass('header-show').removeClass('header-hide');
+            }
+        }, { offset: 0 });
+    },
+    bindControls: function () {
+        var self = this;
+
+        self.animateHeader();
+    },
+}
